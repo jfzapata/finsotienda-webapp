bac30f012080ea47f6b9126418a6e6801b361d6e
Johan Zapata <jfzr9698@gmail.com>
2022-08-16 17:21:33 -0500

    Se agrega interfaz y array de productos

---
+++ src/app/common/constants/products.ts
@@ -0,0 +1,7 @@
+// Own
+// Types
+import { ProductI } from '@app/common/types/interfaces/product';
+
+export const PRODUCTS: ProductI[] = [
+
+];
---
+++ src/app/common/types/interfaces/product.ts
@@ -0,0 +1,5 @@
+export interface ProductI {
+    nombre: string;
+    imagen: string;
+    precio: number;
+} 
