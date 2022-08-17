ecf3d62481208b3aad6cf9860cdbccf6a9d58d54
Johan Zapata <jfzr9698@gmail.com>
2022-08-16 22:29:20 -0500

    Se actualiza json de productos

--- src/app/common/constants/products.ts
+++ src/app/common/constants/products.ts
@@ -4,21 +4,25 @@ import { ProductI } from '@app/common/types/interfaces/product';
 
 export const PRODUCTS: ProductI[] = [
   {
+    id: 1,
     name: 'Arroz Roa 1000g',
     image: '/assets/images/producto_1.png',
     price: 1200
   },
   {
+    id: 2,
     name: 'Coca Cola 250ml',
     image: '/assets/images/producto_2.png',
     price: 1200
   },
   {
+    id: 3,
     name: 'Atún Van Camp 180g',
     image: '/assets/images/producto_3.png',
     price: 1200
   },
   {
+    id: 4,
     name: 'Maíz tostado la especial 40g',
     image: '/assets/images/producto_4.png',
     price: 1200
