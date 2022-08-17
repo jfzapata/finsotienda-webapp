94a4fddd4e8b8d27a8005518d9bad5f414b97e00
Johan Zapata <jfzr9698@gmail.com>
2022-08-16 22:28:09 -0500

    Se consulta cantidad de productos añadidos al carrito

--- src/app/core/components/sales/sales.component.ts
+++ src/app/core/components/sales/sales.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { CartService } from '@app/common/services/cart.service';
 import { ProductsService } from '@app/common/services/products.service';
 
 // Own
@@ -15,7 +16,8 @@ import { ProductI } from '@app/common/types/interfaces/product';
 export class SalesComponent implements OnInit {
   products: ProductI[] = [];
   searchBarControl: FormControl = new FormControl();
-  constructor(private productsService: ProductsService) { }
+  constructor(private productsService: ProductsService,
+    private cartService: CartService) { }
 
   ngOnInit(): void {
     this.searchBarControl.valueChanges.subscribe((val) => this.onSearchBarchValueChange(val));
@@ -26,11 +28,22 @@ export class SalesComponent implements OnInit {
 
   private loadAllProducts(): void {
     this.products = this.productsService.findAll();
-    this.products = this.products.concat(this.products).concat(this.products);
+    this.products = this.products.map(prod => this.setProductExtraInfo(prod));
   }
 
   private onSearchBarchValueChange(value: string): void {
     this.products = this.productsService.findByName(value);
+    this.products = this.products.map(this.setProductExtraInfo);
+  }
+
+  private setProductExtraInfo(product: ProductI): ProductI {
+    const cartItem: ProductI | undefined = this.cartService.getCartItem(product.id);
+    if (cartItem) {
+      product.addedQuantity = cartItem.addedQuantity;
+    } else {
+      product.addedQuantity = 0;
+    }
+    return product;
   }
 
 }
