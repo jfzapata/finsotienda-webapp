671f01e16fed071035dfdd71f826eb73565dc861
Johan Zapata <jfzr9698@gmail.com>
2022-08-17 19:19:07 -0500

    Se hace al navbar fijo

--- src/app/core/layouts/navbar/navbar.component.ts
+++ src/app/core/layouts/navbar/navbar.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, AfterViewInit {
+  @ViewChild('fixedNavbar')
+  fixedNavbar: ElementRef | undefined;
   title = 'Ventas';
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.fixedNavbar)
   }
 
+  ngAfterViewInit(): void {
+    new ResizeObserver(() => {
+      const routeContentWrapper: HTMLElement | null = document.querySelector('.route-content-wrapper');
+      if (routeContentWrapper && this.fixedNavbar) {
+        routeContentWrapper.style.height = `${this.fixedNavbar?.nativeElement.offsetHeight}px`;
+      } else {}
+    }).observe(this.fixedNavbar?.nativeElement);
+  }
 }
