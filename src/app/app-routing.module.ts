688f27567acfb433f1be4e40ed6eda72dad49b45
Johan Zapata <jfzr9698@gmail.com>
2022-08-18 21:26:22 -0500

    Se configura carga de modulos en segundo plano

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [{
   path: 'sales',
@@ -9,10 +9,16 @@ const routes: Routes = [{
   path: '',
   redirectTo: 'sales',
   pathMatch: 'full'
+}, {
+  path: '**',
+  redirectTo: 'sales',
+  pathMatch: 'full'
 }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
